c9c0e203dbe3e4c94e3fa8ebc40b1db57cc6a448
jifakir <jifakir@outlook.com>
2021-09-23 10:44:04 +0600

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import {ref, watch, reactive, onMounted} from 'vue';
-import Todo from './components/Todo.vue';
 
 const todo = ref('');
 const state = reactive({todos: []});
@@ -10,6 +9,12 @@ const state = reactive({todos: []});
     localStorage.setItem('todos', store);
     return todo.value = '';
   }
+  const removeTodo = (index) => {
+        const newState = state.todos.filter((item, idx) => idx !== index);
+        state.todos = newState;
+        localStorage.setItem('todos', JSON.stringify(state.todos));
+    }
+
   onMounted(() => {
     if(localStorage.getItem('todos')){
       const storage = JSON.parse(localStorage.getItem('todos'));
@@ -17,12 +22,6 @@ const state = reactive({todos: []});
       console.log(storage);
     }
   })
-  // watch(
-  //   () => state.todos,
-  //   (todos, prevTodos) => {
-  //     localStorage.setItem('todos', todos)
-  //   }
-  // )
 </script>
 
 <template>
@@ -44,12 +43,15 @@ const state = reactive({todos: []});
         <div class="pl-5 w-1/2">
           <h1 class="text-center mb-5 text-xl">Todo Lists</h1>
           <ul class="w-full h-72 pl-3 pr-4 rounded overflow-y-auto shadow-inner scrollbar scrollbar-thin scrollbar-thumb-yellow-500 scrollbar-thumb-rounded-full scrollbar">
-            <!-- <li class="px-5 py-3 bg-gray-200 bg-opacity-30 rounded shadow-md" v-if="state.todos.length <= 0">Please, add something in todo list.</li>
+            <li v-if="state.todos.length === 0" class="px-5 py-3 bg-gray-200 bg-opacity-30 rounded shadow-md my-2 flex justify-between" >
+                <p class="">Please, add item to see here.</p>
+            </li>
             <li class="px-5 py-3 bg-gray-200 bg-opacity-30 rounded shadow-md my-2 flex justify-between" v-for="(item, idx) in state.todos" :key="idx">
-              <p class="">{{item}}</p>
-              <div class="">icons</div>
-            </li> -->
-            <Todo :todos="state.todos"></Todo>
+                <p class="">{{item}}</p>
+                <div class="cursor-pointer font-medium" @click="removeTodo(idx)">
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"></path></svg>
+                </div>
+            </li>
           </ul>
         </div>
       </div>
